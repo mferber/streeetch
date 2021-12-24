2774eb7e82c1ac692b1d97a76074d1fe4602e785
Matthias Ferber <matthias.ferber@gmail.com>
2021-12-24 15:08:40 -0500

    Duck playing audio for sound fx

--- Streeetch/SoundPlayer.swift
+++ Streeetch/SoundPlayer.swift
@@ -8,6 +8,18 @@ class SoundPlayer {
   let stopSoundURL = Bundle.main.url(forResource: "startstop", withExtension: "wav")!
 
   var player: AVAudioPlayer!
+  let delegate: AVAudioPlayerDelegate!
+
+  init() {
+    do {
+      try audioSession.setCategory(AVAudioSession.Category.playback,
+                                   mode: AVAudioSession.Mode.default,
+                                   options: [.mixWithOthers, .duckOthers])
+    } catch {
+      print("Audio session initialization failed: \(error.localizedDescription)")
+    }
+    delegate = SoundPlayerDelegate(session: audioSession)
+  }
 
   func playCountdownSound() { play(url: countdownSoundURL) }
   func playStartSound() { play(url: startSoundURL) }
@@ -16,9 +28,27 @@ class SoundPlayer {
   private func play(url: URL) {
     do {
       player = try AVAudioPlayer(contentsOf: url)
+      player.delegate = delegate
+      try audioSession.setActive(true)
       player.play()
     } catch {
       print("Sound player failed: \(error.localizedDescription)")
+    }
+  }
+}
+
+class SoundPlayerDelegate: NSObject, AVAudioPlayerDelegate {
+  let audioSession: AVAudioSession
+
+  init(session: AVAudioSession) {
+    self.audioSession = session
+  }
+
+  func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+    do {
+      try audioSession.setActive(false)
+    } catch {
+      print("Audio session deactivation failed: \(error.localizedDescription)")
     }
   }
 }
